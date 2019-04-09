75e0ee2630ce542f69e42f708af62688d5d73994
Adam Koestline <koestline@gmail.com>
2019-04-08 23:23:57 -0500

    changed Prefabs and Animal sawn script and UI

--- AnimalWar1/Assets/Scripts/Animals.cs
+++ AnimalWar1/Assets/Scripts/Animals.cs
@@ -7,11 +7,51 @@ public class Animals : MonoBehaviour
 
     //Attributes
 
-    public float MoveSpeed;
-    public float Health;
-    public float Attack;
-    public float Armor;
-    public bool isEnemy;
+    public float MoveSpeed
+    {
+        get { return MoveSpeed; }
+        set { MoveSpeed = value; }
+    }
+    public float Health
+    {
+        get { return Health; }
+        set { Health = value; }
+    }
+    public float Attack
+    {
+        get { return Attack; }
+        set { Attack = value; }
+    }
+    public float Armor
+    {
+        get { return Armor; }
+        set { Armor = value; }
+    }
+    public bool IsEnemy
+    {
+        get { return IsEnemy; }
+        set { IsEnemy = value; }
+    }
+    public bool CanSwim
+    {
+        get { return CanSwim; }
+        set { CanSwim = value; }
+    }
+    public bool CanFly
+    {
+        get { return CanFly; }
+        set { CanFly = value; }
+    }
+    public bool RangeAttack
+    {
+        get { return RangeAttack; }
+        set { RangeAttack = value; }
+    }
+    public int ResourceCost
+    {
+        get { return ResourceCost; }
+        set { ResourceCost = value; }
+    }
 
     public Transform Wolf;
     public Transform Deer;
@@ -61,38 +101,93 @@ public class Animals : MonoBehaviour
     {
         
     }
+
     public void InstantiateAnimal(AnimalType animal)
     {
         switch (animal)
         {
             case AnimalType.Wolf:
                 Debug.Log("You chose Wolf");
-                Instantiate(Wolf, new Vector3(2.0f,2.0f,2.0f) , Quaternion.identity);
+                var newWolf = Instantiate(Wolf, new Vector3(2.0f,2.0f,2.0f) , Quaternion.identity);
+                var Script = newWolf.GetComponent<animalController>();
+                Script.MoveSpeed = 4;
+                Script.Health = 4;
+                Script.Attack = 4;
+                Script.Armor = 4;
+                Script.IsEnemy = false;
+                Script.CanSwim = false;
+                Script.CanFly = false;
+                Script.RangeAttack = false;
                 break;
 
             case AnimalType.Deer:
                 Debug.Log("You chose Deer");
-                Instantiate(Deer, new Vector3(2.0f, 2.0f, 2.0f), Quaternion.identity);
+                var newDeer = Instantiate(Deer, new Vector3(2.0f, 2.0f, 2.0f), Quaternion.identity);
+                var Script1 = newDeer.GetComponent<animalController>();
+                Script1.MoveSpeed = 4;
+                Script1.Health = 4;
+                Script1.Attack = 4;
+                Script1.Armor = 4;
+                Script1.IsEnemy = false;
+                Script1.CanSwim = false;
+                Script1.CanFly = false;
+                Script1.RangeAttack = false;
                 break;
 
             case AnimalType.Moose:
                 Debug.Log("You chose Moose");
-                Instantiate(Moose, transform.position, Quaternion.identity);
+                var newMoose = Instantiate(Moose, transform.position, Quaternion.identity);
+                var Script2 = newMoose.GetComponent<animalController>();
+                Script2.MoveSpeed = 4;
+                Script2.Health = 4;
+                Script2.Attack = 4;
+                Script2.Armor = 4;
+                Script2.IsEnemy = false;
+                Script2.CanSwim = false;
+                Script2.CanFly = false;
+                Script2.RangeAttack = false;
                 break;
 
             case AnimalType.Bear:
                 Debug.Log("You choose Bear");
-                Instantiate(Bear, transform.position, Quaternion.identity);
+                var newBear = Instantiate(Bear, transform.position, Quaternion.identity);
+                var Script3 = newBear.GetComponent<animalController>();
+                Script3.MoveSpeed = 4;
+                Script3.Health = 4;
+                Script3.Attack = 4;
+                Script3.Armor = 4;
+                Script3.IsEnemy = false;
+                Script3.CanSwim = false;
+                Script3.CanFly = false;
+                Script3.RangeAttack = false;
                 break;
 
             case AnimalType.Fox:
                 Debug.Log("You Chose Fox");
-                Instantiate(Fox, transform.position, Quaternion.identity);
+                var newFox = Instantiate(Fox, transform.position, Quaternion.identity);
+                var Script4 = newFox.GetComponent<animalController>();
+                Script4.MoveSpeed = 4;
+                Script4.Health = 4;
+                Script4.Attack = 4;
+                Script4.Armor = 4;
+                Script4.IsEnemy = false;
+                Script4.CanSwim = false;
+                Script4.CanFly = false;
+                Script4.RangeAttack = false;
                 break;
 
             case AnimalType.Boar:
                 Debug.Log("You chose Boar");
-                Instantiate(Boar, transform.position, Quaternion.identity);
+                var newBoar = Instantiate(Boar, transform.position, Quaternion.identity);
+                var Script5 = newBoar.GetComponent<animalController>();
+                Script5.MoveSpeed = 4;
+                Script5.Health = 4;
+                Script5.Attack = 4;
+                Script5.Armor = 4;
+                Script5.IsEnemy = false;
+                Script5.CanSwim = false;
+                Script5.CanFly = false;
+                Script5.RangeAttack = false;
                 break;
 
                 ////////////////////////
@@ -100,32 +195,86 @@ public class Animals : MonoBehaviour
 
             case AnimalType.Dog:
                 Debug.Log("You chose Dog");
-                Instantiate(Dog, transform.position, Quaternion.identity);
+                var newDog = Instantiate(Dog, transform.position, Quaternion.identity);
+                var eScript = newDog.GetComponent<animalController>();
+                eScript.MoveSpeed = 4;
+                eScript.Health = 4;
+                eScript.Attack = 4;
+                eScript.Armor = 4;
+                eScript.IsEnemy = false;
+                eScript.CanSwim = false;
+                eScript.CanFly = false;
+                eScript.RangeAttack = false;
                 break;
 
             case AnimalType.Cat:
                 Debug.Log("You chose Cat");
-                Instantiate(Cat, transform.position, Quaternion.identity);
+                var newCat = Instantiate(Cat, transform.position, Quaternion.identity);
+                var eScript1 = newCat.GetComponent<animalController>();
+                eScript1.MoveSpeed = 4;
+                eScript1.Health = 4;
+                eScript1.Attack = 4;
+                eScript1.Armor = 4;
+                eScript1.IsEnemy = false;
+                eScript1.CanSwim = false;
+                eScript1.CanFly = false;
+                eScript1.RangeAttack = false;
                 break;
 
             case AnimalType.Bull:
                 Debug.Log("You chose Bull");
-                Instantiate(Bull, transform.position, Quaternion.identity);
+                var newBull = Instantiate(Bull, transform.position, Quaternion.identity);
+                var eScript2 = newBull.GetComponent<animalController>();
+                eScript2.MoveSpeed = 4;
+                eScript2.Health = 4;
+                eScript2.Attack = 4;
+                eScript2.Armor = 4;
+                eScript2.IsEnemy = false;
+                eScript2.CanSwim = false;
+                eScript2.CanFly = false;
+                eScript2.RangeAttack = false;
                 break;
 
             case AnimalType.Rooster:
                 Debug.Log("You choose Rooster");
-                Instantiate(Rooster, transform.position, Quaternion.identity);
+                var newRooster = Instantiate(Rooster, transform.position, Quaternion.identity);
+                var eScript3 = newRooster.GetComponent<animalController>();
+                eScript3.MoveSpeed = 4;
+                eScript3.Health = 4;
+                eScript3.Attack = 4;
+                eScript3.Armor = 4;
+                eScript3.IsEnemy = false;
+                eScript3.CanSwim = false;
+                eScript3.CanFly = false;
+                eScript3.RangeAttack = false;
                 break;
 
             case AnimalType.Rabbit:
                 Debug.Log("You Chose Rabbit");
-                Instantiate(Rabbit, transform.position, Quaternion.identity);
+                var newRabbit = Instantiate(Rabbit, transform.position, Quaternion.identity);
+                var eScript4 = newRabbit.GetComponent<animalController>();
+                eScript4.MoveSpeed = 4;
+                eScript4.Health = 4;
+                eScript4.Attack = 4;
+                eScript4.Armor = 4;
+                eScript4.IsEnemy = false;
+                eScript4.CanSwim = false;
+                eScript4.CanFly = false;
+                eScript4.RangeAttack = false;
                 break;
 
             case AnimalType.Pig:
                 Debug.Log("You chose Pig");
-                Instantiate(Pig, transform.position, Quaternion.identity);
+                var newPig = Instantiate(Pig, transform.position, Quaternion.identity);
+                var eScript5 = newPig.GetComponent<animalController>();
+                eScript5.MoveSpeed = 4;
+                eScript5.Health = 4;
+                eScript5.Attack = 4;
+                eScript5.Armor = 4;
+                eScript5.IsEnemy = false;
+                eScript5.CanSwim = false;
+                eScript5.CanFly = false;
+                eScript5.RangeAttack = false;
                 break;
 
 
@@ -140,5 +289,6 @@ public class Animals : MonoBehaviour
     public void TestInstantiate()
     {
         Instantiate(Wolf, new Vector3(4.0f, 4.0f, 4.0f), Quaternion.identity);
+
     }
 }
---
+++ AnimalWar1/Assets/Scripts/animalController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animalController : MonoBehaviour
+{
+    public float MoveSpeed
+    {
+        get { return MoveSpeed; }
+        set { MoveSpeed = value; }
+    }
+    public float Health
+    {
+        get { return Health; }
+        set { Health = value; }
+    }
+    public float Attack
+    {
+        get { return Attack; }
+        set { Attack = value; }
+    }
+    public float Armor
+    {
+        get { return Armor; }
+        set { Armor = value; }
+    }
+    public bool IsEnemy
+    {
+        get { return IsEnemy; }
+        set { IsEnemy = value; }
+    }
+    public bool CanSwim
+    {
+        get { return CanSwim; }
+        set { CanSwim = value; }
+    }
+    public bool CanFly
+    {
+        get { return CanFly; }
+        set { CanFly = value; }
+    }
+    public bool RangeAttack
+    {
+        get { return RangeAttack; }
+        set { RangeAttack = value; }
+    }
+    public int ResourceCost
+    {
+        get { return ResourceCost; }
+        set { ResourceCost = value; }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MoveCharacter()
+    {
+
+    }
+    private void FightEnemy()
+    {
+
+    }
+    private void InteractWithTile()
+    {
+
+    }
+}
