82d1edecce6aca81e3e6fa09c2d9dbe8bafee1d6
Adam Koestline <koestline@gmail.com>
2019-04-09 20:12:32 -0500

    Added menu functionality

---
+++ AnimalWar1/Assets/Scripts/MenuManger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManger : MonoBehaviour
+{
+
+    public bool isWild = true;
+    public GameObject CanvasTame;
+    public GameObject CanvasWild;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TurnOnMenu();
+    }
+    public void TurnOffMenu()
+    {
+        CanvasWild.SetActive(false);
+        CanvasTame.SetActive(false);
+        //GameObject.Find("UICanvasTame").SetActive(false);
+        //GameObject.Find("UICanvasWild").SetActive(false);
+    }
+    public void TurnOnMenu()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if(isWild)
+            {
+                Debug.Log("called wild cancvas");
+                CanvasWild.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("called tame canvas");
+                CanvasTame.SetActive(true);
+            }          
+            //("UICanvasWild").SetActive(true);
+        }
+    }
+}
